bd48615dd7a44845bcedc77b17905b0814ce2880
eg0r6 <154552104+eg0r6@users.noreply.github.com>
2023-12-25 07:17:06 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,165 @@
+let a = 0
+let b = 1
+let c = 2
+let d = 3
+let e = 4
+let i = 5
+let n = 6
+let f = 7
+let g = 8
+let y = 9
+let x = 10
+document.querySelector('.but').addEventListener('click', () => {
+    let sel = document.querySelector('.select').value;
+    let sel1 = document.querySelector('.select1').value;
+    let sel2 = document.querySelector('.select2').value;
+    let sel3 = document.querySelector('.select3').value;
+    let sel4 = document.querySelector('.select4').value;
+    let sel5 = document.querySelector('.select5').value;
+    let sel6 = document.querySelector('.select6').value;
+    let sel7 = document.querySelector('.select7').value;
+    if (sel<d & sel1>n & sel2<i & sel3<d & sel4>n & sel5>y & sel6<d & sel7<f) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel>c & sel1>n & sel2<i & sel3<d & sel4>n & sel5>y & sel6<d & sel7<f) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel<d & sel1>n & sel2<i & sel3>c & sel4>n & sel5>y & sel6<d & sel7<f) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel>c & sel1>n & sel2<i & sel3>c & sel4>n & sel5>y & sel6<d & sel7<f) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel<d & sel1>n & sel2==i & sel3<d & sel4>n & sel5>y & sel6<d & sel7<f) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel>c & sel1>n & sel2==i & sel3<d & sel4>n & sel5>y & sel6<d & sel7<f) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel<d & sel1>n & sel2==i & sel3>c & sel4>n & sel5>y & sel6<d & sel7<f) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel>c & sel1>n & sel2==i & sel3>c & sel4>n & sel5>y & sel6<d & sel7<f) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel<d & sel1>n & sel2<i & sel3<d & sel4>n & sel5<i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel>c & sel1>n & sel2<i & sel3<d & sel4>n & sel5<i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel<d & sel1>n & sel2<i & sel3>c & sel4>n & sel5<i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel<d & sel1>n & sel2<i & sel3<d & sel4>n & sel5<i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel>c & sel1>n & sel2<i & sel3>c & sel4>n & sel5<i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel<d & sel1>n & sel2<i & sel3>c & sel4>n & sel5<i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel>c & sel1>n & sel2<i & sel3<d & sel4>n & sel5<i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel>c & sel1>n & sel2<i & sel3>c & sel4>n & sel5<i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel<d & sel1>n & sel2==i & sel3<d & sel4>n & sel5==i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel>c & sel1>n & sel2==i & sel3<d & sel4>n & sel5==i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel<d & sel1>n & sel2==i & sel3>c & sel4>n & sel5==i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel<d & sel1>n & sel2==i & sel3<d & sel4>n & sel5==i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel>c & sel1>n & sel2==i & sel3>c & sel4>n & sel5==i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel<d & sel1>n & sel2==i & sel3>c & sel4>n & sel5==i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel>c & sel1>n & sel2==i & sel3<d & sel4>n & sel5==i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel>c & sel1>n & sel2==i & sel3>c & sel4>n & sel5==i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel<d & sel1>n & sel2<i & sel3<d & sel4>n & sel5==i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel>c & sel1>n & sel2<i & sel3<d & sel4>n & sel5==i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel<d & sel1>n & sel2<i & sel3>c & sel4>n & sel5==i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel<d & sel1>n & sel2<i & sel3<d & sel4>n & sel5==i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel>c & sel1>n & sel2<i & sel3>c & sel4>n & sel5==i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel<d & sel1>n & sel2<i & sel3>c & sel4>n & sel5==i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel>c & sel1>n & sel2<i & sel3<d & sel4>n & sel5==i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel>c & sel1>n & sel2<i & sel3>c & sel4>n & sel5==i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel<d & sel1>n & sel2==i & sel3<d & sel4>n & sel5<i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel>c & sel1>n & sel2==i & sel3<d & sel4>n & sel5<i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel<d & sel1>n & sel2==i & sel3>c & sel4>n & sel5<i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel<d & sel1>n & sel2==i & sel3<d & sel4>n & sel5<i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel>c & sel1>n & sel2==i & sel3>c & sel4>n & sel5<i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel<d & sel1>n & sel2==i & sel3>c & sel4>n & sel5<i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel>c & sel1>n & sel2==i & sel3<d & sel4>n & sel5<i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel>c & sel1>n & sel2==i & sel3>c & sel4>n & sel5<i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel<d & sel1<f & sel2>y & sel3<d & sel4>n & sel5<i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel<d & sel1<f & sel2>y & sel3>c & sel4>n & sel5<i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel<d & sel1<f & sel2>y & sel3<d & sel4>n & sel5<i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel<d & sel1<f & sel2>y & sel3>c & sel4>n & sel5<i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+    if (sel<d & sel1<f & sel2>y & sel3<d & sel4>n & sel5==i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel<d & sel1<f & sel2>y & sel3>c & sel4>n & sel5==i & sel6<d & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel<d & sel1<f & sel2>y & sel3<d & sel4>n & sel5==i & sel6>c & sel7>n) {
+        document.querySelector('.div').innerHTML=b;
+    }
+    if (sel<d & sel1<f & sel2>y & sel3>a & sel4>n & sel5==i & sel6>a & sel7>n) {
+        document.querySelector('.div').innerHTML=a;
+    }
+})
